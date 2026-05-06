83be07537151672874ca776952ff583df9881a14
CorinaZL91 <l21010033@itz.edu.mx>
2026-05-06 14:01:15 -0600

    backend finaL V2 CONEXION CON FRONT

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import alertRoutes from "./routes/alert.routes.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -28,9 +29,22 @@ app.use(
 app.use(morgan("common"));
 
 app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL,
+].filter(Boolean) as string[];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        return;
+      }
+
+      callback(new Error("No permitido por CORS"));
+    },
     credentials: true,
   })
 );
